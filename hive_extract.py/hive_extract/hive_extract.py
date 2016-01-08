a3db9283795855cf621d8c6bd85c69acac75924c
Ivan Marin <ivan.smarin@telefonica.com>
2016-01-08 15:52:33 -0200

    first commit

---
+++ hive_extract.py/hive_extract/hive_extract.py
@@ -0,0 +1,27 @@
+#!/bin/env python2.7
+# Ivan Marin
+# Vivo Data Labs
+# ivan.smarin@telefonica.com
+# Converting a hive dump to a semi comma separated value file
+# In the case that the fields have a comma value
+# The input file should be a dump from a query in hive like
+# hive -e 'select * from database.table' > input_file
+
+import csv
+import argparse
+
+parser = argparse.ArgumentParser(
+        description='Convert a Hive dump to semi comma separated file.'
+)
+parser.add_argument('input', help='Hive dump')
+parser.add_argument('output', help='output file')
+args = parser.parse_args()
+
+input_file = args.input
+output_file = args.output
+
+with open(input_file, 'r')as csvfile, open(output_file, 'w') as output:
+    spamreader = csv.reader(csvfile, delimiter='\t')
+    spamwriter = csv.writer(output, delimiter=';')
+    for row in spamreader:
+        spamwriter.writerow(row)
