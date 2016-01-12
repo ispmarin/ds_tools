0d1f8f57484c3cf2ce55fad10e89845d5273c956
Ivan Marin <ivan.smarin@telefonica.com>
2016-01-12 14:23:51 -0200

    mkrepo first commit

---
+++ haversine/haversine.py
@@ -0,0 +1,18 @@
+# Ivan Marin
+# Vivo Data Labs
+# ivan.smarin@telefonica.com
+# 2016-01-11
+
+import numpy as np
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    # Distance between two points
+    # assuming that the input is in WGS84 and is equivalent
+    # to SIRGAS 2000
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+    # haversine formula 
+    a = np.sin((lat2 - lat1 )/2.)**2 + np.cos(lat1) * np.cos(lat2) * np.sin((lon2 - lon1)/2.)**2
+    c = 2. * np.arcsin(np.sqrt(a)) 
+    km = 6367. * c
+    return km
---
+++ mkrepo/generate_wheel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pip wheel -r requirements.txt --wheel-dir=$1
