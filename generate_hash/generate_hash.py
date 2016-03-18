6e831174f429bfba5eeffd317096ed3954c53438
Ivan Marin <ivan.smarin@telefonica.com>
2016-03-18 14:45:43 -0300

    new version of generate_hash

--- generate_hash/generate_hash.py
+++ generate_hash/generate_hash.py
@@ -2,30 +2,45 @@
 Ivan Marin
 Vivo Data Labs
 ivan.smarin@telefonica.com
+2016-03-18
+
+Generate SHA 256 hashes from fields in a CSV file.
 """
 import hashlib
 import csv
 import argparse
 import base64
 import os
+import sys
 
 
-def hash_function(input_string):
+def hash_function_base64(input_string):
     """
     Generates a SHA 256 hash from a string and encodes it
     in base64.
     :param input_string:
-    :return: base64(sha256(string))
+    :return: base64(sha256(string).upper())
     """
     return base64.b64encode(
         hashlib.sha256(
             input_string
-        ).hexdigest()
+        ).hexdigest().upper()
     )
+
+def hash_function(input_string):
+    """
+    Generates a SHA 256 hash from a string
+    :param input_string:
+    :return: (sha256(string).upper())
+    """
+    return hashlib.sha256(
+            input_string
+        ).hexdigest().upper()
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Convert a list of numbers to SHA 256 hashes encoded in base64.'
+        description='Convert a list of fields in a CSV file to SHA 256 hashes.'
     )
     parser.add_argument(
         '-i',
@@ -41,27 +56,52 @@ if __name__ == '__main__':
         type=str,
         required=True
     )
+    parser.add_argument(
+        '-n',
+        '--name_field',
+        help='Name of the field to be read.',
+        type=str,
+        required=True
+    )
+
+    parser.add_argument(
+        '-f',
+        '--out_field',
+        help='Name of the field to be written.',
+        type=str,
+        required=True
+    )
+
+    parser.add_argument(
+        '-b',
+        '--base64_flag',
+        help='If the hash SHA 256 is encoded on Base64 or not. Default is no.',
+        type=str,
+        choices=['yes', 'no'],
+        default='no'
+    )
     args = parser.parse_args()
     input_file = args.input_file
     output_file = args.output_file
+    field_name = args.name_field
+    out_field = args.out_field
+    base64_flag = args.base64_flag
 
     with open(input_file, 'rUb') as f, open(output_file, 'wb') as out_f:
         reader = csv.DictReader(f)
-        # here sets which columns should be on the output file
-        fieldnames = reader.fieldnames + ['hash_field']
-        writer = csv.DictWriter(out_f, fieldnames, lineterminator=os.linesep)
-        writer.writeheader()
+        fieldnames = reader.fieldnames + [out_field] 
+        writer = csv.writer(out_f, fieldnames, lineterminator=os.linesep)
+        writer.writerow(fieldnames)
+        if base64_flag == 'yes':
+            hash_func = hash_function_base64
+        else:
+            hash_func = hash_function
         for row in reader:
             try:
-                data_to_hash = row.get('cpf')
-                print(data_to_hash)
+                data_to_hash = row.get(field_name)
                 writer.writerow(
-                    dict(
-                        row,
-                        hash_field=hash_function(
-                            data_to_hash
+                        [data_to_hash, hash_func(data_to_hash)]
                         )
-                    )
-                )
-            except ValueError:
+            except ValueError as e:
                 print "failure to read row ", row
+                print e
--- generate_hash/manual.md
+++ generate_hash/manual.md
@@ -9,7 +9,12 @@ O script Python gera hashes usando o algoritmo [SHA-256](https://en.wikipedia.or
 e faz a codificação destes hashes em [Base64](https://en.wikipedia.org/wiki/Base64).
 
 ## Algoritmo
-O script executa as seguintes operações:
+O script é capaz de executar as seguintes operações:
+
+- gerar hash em SHA 256 e fazer encoding em Base64
+- gerar hash em SHA 256
+
+O algoritmo básico é describo por
 
 ```
 para linha em arquivo:
@@ -21,9 +26,19 @@ para linha em arquivo:
 O script requer Python versão 2.7. Para executar, em um terminal utilize o comando
 
 ```
-python generate_hash.py -i test.csv -o out.csv
+python generate_hash.py -i test.csv -o out.csv -n cpf -f hashed_cpf -b yes 
 ```
-os quais `test.csv` é o arquivo de entrada com o nome do tipo de dado e os dados a serem gerados hashes e `out.csv` é
-o arquivo de saída com os valores originais de entrada e uma coluna adicional, chamada de `hash`.
+
+As opções são:
+
+- `-i`: Arquivo de entrada com os dados a serem gerados os hashes
+- `-o`: Arquivo de saída, com os dados originais e os dados transformados em hash
+- `-f`: Nome do campo do arquivo de entrada que contém os dados a serem gerados hash
+- `-f`: Nome do campo para os hashes gerados
+- `-b`: `yes` se o hash SHA 256 deve ser encodado em Base64 e `no` se o hash deve ser somente em SHA 256. O padrão é 'no'
+
+O arquivo `test.csv` representa o arquivo de entrada com o nome do tipo de dado e os dados a serem gerados hashes e `out.csv` é
+o arquivo de saída com os valores originais de entrada e uma coluna adicional dada por `-f`.
 
 O executável do Python deve estar no caminho do terminal.
+
